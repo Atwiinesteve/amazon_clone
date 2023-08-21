d24ddd7da82457c5a52eec944035aeaddd250777
Kiiza Atwiine Stephen <atwiinestephen123@gmail.com>
2023-08-21 16:12:12 +0300

    completed Navbar

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import TopNavBar from '../components/TopNavBar'
+import Navbar from '../components/Navbar'
 
 export default function Home() {
   return (
     <React.Fragment>
         <TopNavBar />
+        <Navbar />
     </React.Fragment>
   )
 }
