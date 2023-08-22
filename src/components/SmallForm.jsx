ac5462f0d4bc1fc225a8d6a38342a46303c67936
Kiiza Atwiine Stephen <atwiinestephen123@gmail.com>
2023-08-22 15:01:25 +0300

    finished with first row

---
+++ src/components/SmallForm.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function SmallForm() {
+	return (
+		<React.Fragment>
+			<div className="bg-white w-full h-[150px] pl-2 pr-2 flex flex-col justify-center items-center gap-2">
+				<h2 className="font-bold text-xl">Sign in for the best experience</h2>
+				<button className="bg-yellow-400 text-black text-sm capitalize w-full p-1 hover:bg-yellow-600 hover:cursor-pointer">
+					sign in securely
+				</button>
+			</div>
+		</React.Fragment>
+	);
+}
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,18 +1,34 @@
-import React from 'react'
-import TopNavBar from '../components/TopNavBar'
-import Navbar from '../components/Navbar'
-import HeaderWithCarousel from '../components/CarouselArea'
-import HomeAndKitchen from '../components/Home&Kitchen'
+import React from "react";
+import TopNavBar from "../components/TopNavBar";
+import Navbar from "../components/Navbar";
+import HeaderWithCarousel from "../components/CarouselArea";
+import HomeAndKitchen from "../components/Home&Kitchen";
+import Toys from "../components/Toys";
+import Gaming from "../components/Gaming";
+import SmallForm from "../components/SmallForm";
 
 export default function Home() {
-  return (
-    <React.Fragment>
-        <TopNavBar />
-        <Navbar />
-        <main className="ml-[2rem] mr-[2rem]">
-          <HeaderWithCarousel />
-          <HomeAndKitchen />
-        </main>
-    </React.Fragment>
-  )
+	return (
+		<React.Fragment>
+			<TopNavBar />
+			<Navbar />
+			<main className="ml-[2rem] mr-[2rem]">
+				<HeaderWithCarousel />
+				<div className="flex flex-row items-center justify-center gap-3 -mt-[20rem] z-30">
+					<Gaming />
+					<Toys />
+					<HomeAndKitchen />
+					<div className="flex flex-col items-center justify-start gap-3">
+						<SmallForm />
+						<img className=" object-cover"
+							src={
+								"https://images-na.ssl-images-amazon.com/images/G/01/AmazonExports/Fuji/2020/October/Fuji_D2_45M_en_US_1x._CB418309979_.jpg"
+							}
+							alt=""
+						/>
+					</div>
+				</div>
+			</main>
+		</React.Fragment>
+	);
 }
